2e8398292037853f354774cc52bd406ac3fbb8dc
BioNeed <cianis2002@gmail.com>
2022-04-26 20:38:37 +0300

    lll

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    public Color highlightColor = Color.cyan;
+
+
+    public void OnMouseOver()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+            sprite.color = highlightColor;
+    }
+
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite.color != null)
+            sprite.color = Color.white;
+    }
+
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    }
+
+    public void OnMouseUp()
+    {
+        transform.localScale = Vector3.one;
+        if (targetObject != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
